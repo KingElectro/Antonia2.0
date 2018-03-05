8ff899ceecbc0eb1b9233a034e7391af5e5145fc
KingElectro <37087831+KingElectro@users.noreply.github.com>
2018-03-05 22:50:37 +0000

    Update musicbot.js

--- musicbot.js
+++ musicbot.js
@@ -12,7 +12,6 @@
 				//    You should have received a copy of the GNU General Public License       //   
 				//    along with this program.  If not, see <http://www.gnu.org/licenses/>.   //   
 				////////////////////////////////////////////////////////////////////////////////
-client.login(process.env.BOT_TOKEN);
 const Discord = require("discord.js");
 const fs = require("fs");
 const ytdl = require("ytdl-core");
